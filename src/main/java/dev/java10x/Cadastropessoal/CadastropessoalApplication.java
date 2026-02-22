0da4e087524f814c31f8202644aab397d86ca563
Filipinas0990 <filipeegustavo0990@gmail.com>
2026-02-22 15:25:49 -0300

    primeiro commit

---
+++ src/main/java/dev/java10x/Cadastropessoal/CadastropessoalApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.Cadastropessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastropessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastropessoalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/Cadastropessoal/CadastropessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.Cadastropessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastropessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
